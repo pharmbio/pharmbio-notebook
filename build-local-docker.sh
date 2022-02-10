5faf1070f174374000203dc75fa93fdb224f3046
David Holmberg <david.holmberg@farmbio.uu.se>
2022-02-10 14:30:34 +0100

    Finished automated build-script. This docker-file now builds a Docker-Image with CUDA11.2, TF2.8.0, and Pytorch 1.9. It runs on Ubuntu 20.04 and python 3.8.x

--- build-local-docker.sh
+++ build-local-docker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-#git_branch="$(git branch --show-current)"
-#tag=$git_branch
-#tensorflow_version="${git_branch#tf-}" # remove prefix
-#tensorflow_version="${tensorflow_version%-*}" # remove suffix 
-#image="pharmbio-notebook"
-#echo "tensorflow_version=$tensorflow_version"
-#echo "tag=$tag"
-#echo "image=$image"
+git_branch="$(git branch --show-current)"
+tag=$git_branch
+tensorflow_version="${git_branch#tf-}" # remove prefix
+tensorflow_version="${tensorflow_version%-*}" # remove suffix
+tensorflow_version="${tensorflow_version%-*}" 
+image="pharmbio-notebook"
+echo "tensorflow_version=$tensorflow_version"
+echo "tag=$tag"
+echo "image=$image"
 
 # Build docker image for this container.
-docker build -t pharmbio/pharmbio-notebook:tf-2.8.0-pytorch-gpu . --build-arg BASE_IMAGE=tensorflow/tensorflow:2.8.0-gpu-jupyter --no-cache
+#docker build -t pharmbio/pharmbio-notebook:tf-2.8.0-pytorch-gpu . --build-arg #BASE_IMAGE=tensorflow/tensorflow:2.8.0-gpu-jupyter --no-cache
+
+docker build -t pharmbio/$image:${tag} . --build-arg BASE_IMAGE=tensorflow/tensorflow:${tensorflow_version}-gpu
