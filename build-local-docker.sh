73bd7482fca8356ecf448e72d84964a646ab5406
David Holmberg <david.holmberg@farmbio.uu.se>
2022-02-16 14:56:36 +0100

    updated buildfile

--- build-local-docker.sh
+++ build-local-docker.sh
@@ -4,7 +4,7 @@ git_branch="$(git branch --show-current)"
 tag=$git_branch
 tensorflow_version="${git_branch#tf-}" # remove prefix
 tensorflow_version="${tensorflow_version%-*}" # remove suffix
-tensorflow_version="${tensorflow_version%-*}" 
+#tensorflow_version="${tensorflow_version%-*}" 
 image="pharmbio-notebook"
 echo "tensorflow_version=$tensorflow_version"
 echo "tag=$tag"
@@ -13,4 +13,7 @@ echo "image=$image"
 # Build docker image for this container.
 #docker build -t pharmbio/pharmbio-notebook:tf-2.8.0-pytorch-gpu . --build-arg #BASE_IMAGE=tensorflow/tensorflow:2.8.0-gpu-jupyter --no-cache
 
-docker build -t pharmbio/$image:${tag} . --build-arg BASE_IMAGE=tensorflow/tensorflow:${tensorflow_version}-gpu
+#docker build -t pharmbio/$image:${tag} . --build-arg BASE_IMAGE=tensorflow/tensorflow:${tensorflow_version}-gpu
+# Build CPU and GPU version of this tensorflow docker
+docker build -t pharmbio/$image:$tag . --build-arg BASE_IMAGE=tensorflow/tensorflow:${tensorflow_version}-jupyter
+docker build -t pharmbio/$image:${tag}-gpu . --build-arg BASE_IMAGE=tensorflow/tensorflow:${tensorflow_version}-gpu-jupyter
