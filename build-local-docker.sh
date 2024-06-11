075c59082c54e9baea3a2a77c6fcbbfce1cfa49c
david <david.holmberg@farmbio.uu.se>
2024-06-11 15:41:51 +0200

    updated to buildx

--- build-local-docker.sh
+++ build-local-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xv
+#set -xv
 
 git_branch="$(git branch --show-current)"
 tag=$git_branch
@@ -9,13 +9,15 @@ image="pharmbio-notebook"
 echo "tensorflow_version=$tensorflow_version"
 echo "tag=$tag"
 echo "image=$image"
-
+#switch to buildx build
 # Build docker image for this container.
-docker build -t ghcr.io/pharmbio/$image:$tag-devel \
+docker buildx build -t ghcr.io/pharmbio/$image:$tag-devel \
              --build-arg BASE_IMAGE=tensorflow/tensorflow:${tensorflow_version}-jupyter \
-             --build-arg FRAMEWORK=cpu .
-docker build -t ghcr.io/pharmbio/$image:${tag}-gpu-devel \
+             --build-arg FRAMEWORK=cpu \
+             -f ./docker/Dockerfile . || exit 1
+docker buildx build -t ghcr.io/pharmbio/$image:${tag}-gpu-devel \
              --build-arg BASE_IMAGE=tensorflow/tensorflow:${tensorflow_version}-gpu-jupyter \
-             --build-arg FRAMEWORK=cuda .
+             --build-arg FRAMEWORK=cuda \
+             -f docker/Dockerfile . || exit 1
 
-docker push "ghcr.io/pharmbio/$image:$tag-devel"
+#docker push "ghcr.io/pharmbio/$image:$tag-devel"
